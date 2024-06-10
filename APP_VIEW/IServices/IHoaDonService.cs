32e9be4b5bd67835270b220bbe3da0d413acef57
duy quan <toanxuandepzai@gmail.com>
2024-06-10 17:51:55 +0700

    HoaDonDetails and delete thay doi trang thai

---
+++ APP_VIEW/IServices/IHoaDonService.cs
@@ -0,0 +1,12 @@
+﻿using APP_DATA.DTO;
+using APP_DATA.Models;
+
+namespace APP_VIEW.IServices
+{
+    public interface IHoaDonService
+    {
+        Task<List<HoaDonResponce>> GetAllHoaDons();
+        Task<HoaDon?> GetHoaDonById(Guid? id);
+        Task<bool> DeleteHang(Guid id);
+    }
+}

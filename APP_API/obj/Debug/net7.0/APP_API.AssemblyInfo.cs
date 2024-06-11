b2423de23e7f688da6673a8666c0b63321156c1b
duy quan <toanxuandepzai@gmail.com>
2024-06-11 22:40:34 +0700

    HoaDonMuaLai

--- APP_API/obj/Debug/net7.0/APP_API.AssemblyInfo.cs
+++ APP_API/obj/Debug/net7.0/APP_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APP_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068650bd17489e40b6be798532f078d70e9c9c39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fae624bb2379dded2c7793e974f8e05c5c0cdb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("APP_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APP_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
